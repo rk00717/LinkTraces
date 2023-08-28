09ac6e7372661f78f6c48676b20708cad630518a
rk00717 <58892434+rk00717@users.noreply.github.com>
2023-08-29 02:03:42 +0530

    Added link Copy Feature

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 function add_link_trace(title, link){
-    var button = document.createElement("div");
-    button.className = "link_button";
-    button.onclick = function(){
+    var link_container = document.createElement("div");
+    link_container.className = "link_container"
+
+    var link_button = document.createElement("button");
+    link_button.className = "link_button";
+    link_button.onclick = function(){
         window.open(link);
     };
-    button.innerHTML = '<span class="links">'+title+'</span>';
+    
+    var copy_button = document.createElement("button");
+    copy_button.className = "link_copy_button";
+    copy_button.onclick = function(){
+        copy_to_clipboard(link);
+    }
 
+    // copy_button.innerHTML = "&#x2398;";
+    link_button.innerHTML = title;
+    
     var container = document.getElementById("links_list");
-    container.append(button);
+    link_container.append(link_button);
+    link_container.append(copy_button);
+    container.append(link_container);
 }
 
 function copy_to_clipboard(link){
