6ec257c935035e59ce258eb4f864192372f02145
rk00717 <58892434+rk00717@users.noreply.github.com>
2023-12-03 15:20:14 +0530

    Update constant.js

--- js/constant.js
+++ js/constant.js
@@ -1,4 +1,4 @@
 const YOUTUBE_LINK = "https://www.youtube.com/@ronikmarghade4849";
-const ITCHIO_LINK = "https://rk00717.itch.io/";
+const ITCHIO_LINK = "https://rk00717.itch.io";
 const GITHUB_LINK = "https://github.com/rk00717";
-const LINKEDN_LINK = "https://www.linkedin.com/in/ronik-marghade-5174131bb/";
+const LINKEDN_LINK = "https://www.linkedin.com/in/rk00717";
